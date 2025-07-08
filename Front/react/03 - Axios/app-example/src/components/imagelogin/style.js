478f5b53d9a863f5ea809ec886d3fafac4fc60b6
gileduardo <gilgea@gmail.com>
2025-07-08 11:39:56 -0300

    React App - Exemplo

---
+++ Front/react/03 - Axios/app-example/src/components/imagelogin/style.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+import logo_depen from '../../images/logo_depen.png';
+
+export const Container = styled.div`
+    
+    background-image: url( ${logo_depen});
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-position: center;
+    border-left: 1px solid #CCC;
+
+    @media (max-width: 800px) {
+        display: none;
+    }
+`

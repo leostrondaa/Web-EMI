57365524b72c47941e636f19094d386f5f1198cc
gileduardo <gilgea@gmail.com>
2025-09-15 15:29:46 -0300

    Adonis - Auth_Token_Bouncer_Policy

---
+++ Back/AdonisJS/03 - Auth_Token_Bouncer_Policy/app/models/user.ts
@@ -0,0 +1,42 @@
+import { DateTime } from 'luxon'
+import hash from '@adonisjs/core/services/hash'
+import { compose } from '@adonisjs/core/helpers'
+import { BaseModel, column, belongsTo } from '@adonisjs/lucid/orm'
+import { withAuthFinder } from '@adonisjs/auth/mixins/lucid'
+import { DbAccessTokensProvider } from '@adonisjs/auth/access_tokens'
+import type { BelongsTo } from '@adonisjs/lucid/types/relations'
+import Papel from '#models/papel'
+
+const AuthFinder = withAuthFinder(() => hash.use('scrypt'), {
+  uids: ['email'],
+  passwordColumnName: 'password',
+})
+
+export default class User extends compose(BaseModel, AuthFinder) {
+  @column({ isPrimary: true })
+  declare id: number
+
+  @column()
+  declare fullName: string | null
+
+  @column()
+  declare email: string
+
+  @column({ serializeAs: null })
+  declare password: string
+
+  @column()
+  declare papel_id: number
+
+  @column.dateTime({ autoCreate: true })
+  declare createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  declare updatedAt: DateTime | null
+
+  static accessTokens = DbAccessTokensProvider.forModel(User)
+
+  // Relacionamentos
+  @belongsTo(() => Papel, { foreignKey: 'curso_id' })
+  declare papel: BelongsTo<typeof Papel>
+}

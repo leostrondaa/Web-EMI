33ac8fdbd58916a728f5d83b8c4a44a9f9d65da9
leostrondaa <yaayleo@gmail.com>
2025-09-29 16:49:30 -0300

    we4

---
+++ hello-world/app/policies/post_policy.ts
@@ -0,0 +1,8 @@
+import User from '#models/user'
+import Post from '#models/post'
+import { BasePolicy } from '@adonisjs/bouncer'
+import type { AuthorizerResponse } from '@adonisjs/bouncer/types'
+
+export default class PostPolicy extends BasePolicy {
+  
+}
